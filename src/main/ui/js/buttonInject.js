257c77dc571a81fc2b172209881c58144786303c
Mario Enrico Ragucci <mario@whisperedshouts.de>
2017-10-28 18:40:35 +0200

    minor fix
    Signed-off-by: Mario Enrico Ragucci <mario@whisperedshouts.de>

--- src/main/ui/js/buttonInject.js
+++ src/main/ui/js/buttonInject.js
@@ -1,9 +1,9 @@
-var url = SailPoint.CONTEXT_PATH + '/plugins/pluginPage.jsf?pn=password-pronunciation-plugin';
 jQuery(document).ready(function(){
+	var passwordPronunciationPluginUrl = SailPoint.CONTEXT_PATH + '/plugins/pluginPage.jsf?pn=password-pronunciation-plugin';
     jQuery("ul.navbar-right li:first")
         .before(
             '<li class="dropdown">' +
-            '		<a href="' + url + '" tabindex="0" role="menuitem" title="Pronunciation Settings">' +
+            '		<a href="' + passwordPronunciationPluginUrl + '" tabindex="0" role="menuitem" title="Pronunciation Settings">' +
             '			<i role="presenation" class="fa fa-key fa-lg example"></i>' +
             '		</a>' +
             '</li>'
